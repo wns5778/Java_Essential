45a1b53f75cb7c49239b4c9001113d8d45086b56
wns5778 <wns5778@naver.com>
2021-07-28 23:13:39 +0900

    day 11

---
+++ src/chapter6/chapter6_RandomNumber.java
@@ -0,0 +1,27 @@
+package chapter6;
+
+//1에서 3까지 난수 3개 생성뒤 3개가 같은수가 나올때까지 반복, 성공하면 "성공"출력
+
+public class chapter6_RandomNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array[] = new int[3];
+		while(true) {
+			for(int i=0;i<3;i++) {
+				array[i] = (int)(Math.random()*3+1);
+				System.out.print(array[i]);
+			}
+			System.out.println("");
+			if(array[0] == array[1] && array[1]== array[2]) {
+				System.out.print("성공");
+				break;
+			}
+
+		}
+		
+
+	}
+
+}
